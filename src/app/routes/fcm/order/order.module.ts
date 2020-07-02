20a4c667eab49afce467982ff665aa7610cd66b7
Sherrylong <>
2020-07-02 19:50:49 +0800

    增加组件

--- src/app/routes/fcm/order/order.module.ts
+++ src/app/routes/fcm/order/order.module.ts
@@ -4,18 +4,12 @@ import { SharedModule } from '@shared';
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
-
-const COMPONENTS = [
-  OrderListComponent,
-  OrderDetailComponent
-];
+import { CoDocumentComponent } from './co-document/co-document.component';
+import { AddOrderComponent } from './add-order/add-order.component';
+const COMPONENTS = [OrderListComponent, OrderDetailComponent, CoDocumentComponent, AddOrderComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [
-    SharedModule,
-    CommonModule,
-    OrderRoutingModule
-  ]
+  imports: [SharedModule, CommonModule, OrderRoutingModule],
 })
-export class OrderModule { }
+export class OrderModule {}
