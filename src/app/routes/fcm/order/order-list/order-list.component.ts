046e448c2dfddf424bc8161cfd181e5f91f3a166
liuxiao <lx@LONGWIN201933>
2020-07-02 20:29:02 +0800

    确认入仓弹窗

--- src/app/routes/fcm/order/order-list/order-list.component.ts
+++ src/app/routes/fcm/order/order-list/order-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { CoDocumentComponent } from '../co-document/co-document.component';
+import {FormBuilder, FormGroup} from "@angular/forms";
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.less'],
 })
 export class OrderListComponent implements OnInit {
-  listOfData: Person[] = [
+  listOfData: any[] = [
     {
       key: '1',
       name: 'John Brown',
@@ -26,7 +26,35 @@ export class OrderListComponent implements OnInit {
       address: 'Sidney No. 1 Lake Park',
     },
   ];
-  constructor() {}
 
-  ngOnInit(): void {}
+  validateForm: FormGroup;
+  openBookings = false;
+  date = null;
+  loading = false;
+
+  constructor( public fb : FormBuilder ) {}
+
+  onStartChange() {
+    this.validateForm.patchValue({ creatDate: null });
+  }
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      creatDate: [null],
+    });
+  }
+  closeBooking(){
+    this.openBookings = false;
+  }
+
+  showBooking(): void {
+    this.openBookings = true;
+    this.validateForm.patchValue({ creatDate: null });
+  }
+
+  handleOk(): void {
+    this.loading = true;
+    console.log(this.validateForm.value.creatDate);
+    this.closeBooking();
+    this.loading = false;
+  }
 }
