c1f31d076d560e033201fc8561cc8c58da8a7cdc
Sherrylong <>
2020-07-02 20:13:18 +0800

    静态页面

---
+++ src/app/routes/fcm/order/add-order/add-order.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-order',
+  templateUrl: './add-order.component.html',
+  styleUrls: ['./add-order.component.less'],
+})
+export class AddOrderComponent implements OnInit {
+  validateForm!: FormGroup;
+  isVisible = false;
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  get isHorizontal(): boolean {
+    return this.validateForm.controls.formLayout?.value === 'horizontal';
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      formLayout: ['horizontal'],
+      fieldA: [null, [Validators.required]],
+      filedB: [null, [Validators.required]],
+    });
+  }
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+}
--- src/app/routes/fcm/order/order-list/order-list.component.ts
+++ src/app/routes/fcm/order/order-list/order-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CoDocumentComponent } from '../co-document/co-document.component';
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.less']
+  styleUrls: ['./order-list.component.less'],
 })
 export class OrderListComponent implements OnInit {
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
