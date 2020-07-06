f164d419c04d32b118ac70d35191071817185930
Sherrylong <>
2020-07-06 16:53:31 +0800

    新增订单-预报

--- src/app/routes/fcm/order/order-list/order-list.component.ts
+++ src/app/routes/fcm/order/order-list/order-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AddOrderComponent } from '../add-order/add-order.component';
+import { STColumn } from '@co/cbc';
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
@@ -11,25 +12,39 @@ export class OrderListComponent implements OnInit {
   isAddVisible = false;
   isVisible = false;
   date = null;
-  listOfData = [
+  listOfData: [
     {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
+      id: '1';
+      img: 'John Brown';
+      email: 32;
+      address: 'New York No. 1 Lake Park';
+      phone: '1212';
+      date: 'date';
     },
     {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
+      id: '2';
+      img: 'John Brown';
+      email: 32;
+      address: 'New York No. 1 Lake Park';
+      phone: '1212';
+      date: 'date';
     },
     {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
+      id: '3';
+      img: 'John Brown';
+      email: 32;
+      address: 'New York No. 1 Lake Park';
+      phone: '1212';
+      date: 'date';
     },
+  ];
+  columns: STColumn[] = [
+    { title: '编号', index: 'id', width: 80 },
+    { title: '头像', type: 'img', width: 80, index: 'picture.thumbnail' },
+    { title: '邮箱', index: 'email', width: 80 },
+    { title: '电话', index: 'phone', width: 80 },
+    { title: { text: '佣金', optional: '（单位：元）', optionalHelp: '计算公式=订单金额 * 0.6%' }, index: 'price', type: 'currency' },
+    { title: '注册时间', type: 'date', index: 'registered' },
   ];
   constructor() {}
 
