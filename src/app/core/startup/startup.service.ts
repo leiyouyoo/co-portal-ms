ba6ce2b743b400fa36328ac47571ad92b9ae24f8
xianxiaodong <53>
2020-08-03 08:51:55 +0800

    菜单项通过菜单服务获取

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -37,6 +37,7 @@ export class StartupService {
     // only works with promises
     // https://github.com/angular/angular/issues/15088
     return new Promise((resolve) => {
+      debugger;
       this.httpClient.get(`assets/i18n/${this.i18n.defaultLang}.json`).subscribe((langData) => {
         // 设置语言数据
         this.translate.setTranslation(this.i18n.defaultLang, langData);
@@ -44,7 +45,7 @@ export class StartupService {
         this.translate.setDefaultLang(this.i18n.defaultLang);
 
         this.httpClient
-          .get(CoConfigManager.getValue('serverUrl') + '/platform/Session/GetCurrentUserConfiguration')
+          .get(CoConfigManager.getValue('serverUrl') + '/platform/Session/GetCurrentUserConfiguration?client=ICP_Web')
           .pipe(
             // 接收其他拦截器后产生的异常消息
             catchError((res) => {
@@ -66,154 +67,30 @@ export class StartupService {
               this.setupAclData(appData);
 
               // 初始化菜单
-              // const menus = this.convertMenus(appData.nav.menus.MainMenu.items as any[])
-              // this.menuService.add(menus);
-              const frmMenus = [
-                {
-                  name: 'FRM_OceanFees',
-                  icon: 'iconsetting',
-                  displayName: '海运运价',
-                  order: 20,
-                  url: '/rates/#/ocean',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FRM_TruckFees',
-                  icon: 'iconsetting',
-                  displayName: '拖车价格',
-                  order: 21,
-                  url: '/rates/#/truckingRates',
-                  customData: { type: 0, id: 'ba8e5590-d305-4e41-d314-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FRM_LocalFees',
-                  icon: 'iconsetting',
-                  displayName: '本地费用',
-                  order: 22,
-                  url: '/rates/#/localfee',
-                  customData: { type: 0, id: '343bc33d-f1fd-4c28-d315-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FRM_Quotes',
-                  icon: 'iconsetting',
-                  displayName: '询价',
-                  order: 23,
-                  url: '/rates/#/enquiry',
-                  customData: { type: 0, id: '4e103430-9069-41cc-d316-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FRM_CustomerInquiries',
-                  icon: 'iconsetting',
-                  displayName: '运价',
-                  order: 25,
-                  url: '/rates/#/inquiriesRates',
-                  customData: { type: 0, id: '03a83907-2996-4f69-99c8-f83aeabf647e' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-              ];
+              let menus = [];
 
-              const crmMenus = [
-                {
-                  name: 'PLATFORM_JOB',
-                  icon: 'iconsetting',
-                  displayName: '职务管理',
-                  order: 20,
-                  url: '/platform/job',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'PLATFORM_POSITION',
-                  icon: 'iconsetting',
-                  displayName: '职位管理',
-                  order: 20,
-                  url: '/platform/position',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FCM_ORDER',
-                  icon: 'iconsetting',
-                  displayName: '订单管理',
-                  order: 20,
-                  url: '/fcm/order',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FCM_BILL',
-                  icon: 'iconsetting',
-                  displayName: '账单管理',
-                  order: 20,
-                  url: '/fcm/bill',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-              ];
+              if (appData.nav?.menus?.MainMenu?.items?.length > 0) {
+                menus = this.convertMenus(appData.nav.menus.MainMenu.items[0].items as any[]);
+              }
 
               const favoritesMenus = [
                 {
-                  name: 'PLATFORM_JOB',
-                  icon: 'iconsetting',
-                  displayName: '职务管理',
+                  name: 'FCM_BILLS',
+                  icon: 'iconmenu-default',
+                  displayName: '账单管理',
                   order: 20,
-                  url: '/platform/job',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
+                  url: '/fcm/bill/billlist',
                   target: null,
                   isEnabled: true,
                   isVisible: true,
                   items: [],
                 },
                 {
-                  name: 'PLATFORM_POSITION',
-                  icon: 'iconsetting',
-                  displayName: '职位管理',
-                  order: 20,
-                  url: '/platform/position',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
-                  target: null,
-                  isEnabled: true,
-                  isVisible: true,
-                  items: [],
-                },
-                {
-                  name: 'FCM_ORDER',
-                  icon: 'iconsetting',
+                  name: 'FCM_ORDERS',
+                  icon: 'iconmenu-default',
                   displayName: '订单管理',
                   order: 20,
                   url: '/fcm/order/orderlist',
-                  customData: { type: 0, id: '5fa265c8-54ed-4568-d313-08d7e2a23614' },
                   target: null,
                   isEnabled: true,
                   isVisible: true,
@@ -224,18 +101,7 @@ export class StartupService {
               this.menuService.add([
                 {
                   key: 'menus',
-                  children: [
-                    {
-                      key: 'FRM',
-                      text: '运价管理',
-                      children: this.convertMenus(frmMenus),
-                    },
-                    {
-                      key: 'PLATFORM',
-                      text: '平台管理',
-                      children: this.convertMenus(crmMenus),
-                    },
-                  ],
+                  children: menus,
                 },
                 {
                   key: 'favorites',
@@ -275,6 +141,7 @@ export class StartupService {
     const inFn = (data: NzSafeAny[], parent: NzSafeAny, newMenus: any[]) => {
       for (const item of data) {
         const newMenu = {
+          key: item.name,
           text: item.displayName,
           il8N: item.displayName,
           link: item.url,
