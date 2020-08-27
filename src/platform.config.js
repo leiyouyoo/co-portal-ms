2e6691751417132e26b52dd8a5dd9f3bd2f64c98
xianxiaodong <53>
2020-08-27 18:45:38 +0800

    调整es支持

--- src/platform.config.js
+++ src/platform.config.js
@@ -1,7 +1,11 @@
 (function () {
   var protocol = 'http';
-  var serverHost = 'dev-api.cityocean.com';
-  var apiPort = '20001';
+  // var serverHost = 'dev-api.cityocean.com';
+  // var apiPort = '20001';
+  // var signalRPort = '20002';
+ 
+  var serverHost = 'test-api.cityocean.com';
+  var apiPort = '20006';
   var signalRPort = '20002';
   var serverUrlPrefix = protocol + '://' + serverHost;
   var apiUrlPrefix = serverUrlPrefix + ':' + apiPort;
@@ -13,7 +17,7 @@
     serverUrl: apiUrlPrefix,
     signalRUrl: serverUrlPrefix + ':' + signalRPort,
     storeUrl: apiUrlPrefix,
-    loginUrl: '/#/passport/login',
+    loginUrl: '/passport/login',
     uploadUrl: apiUrlPrefix + '/Storage/File/Upload',
     downloadUrl: apiUrlPrefix + '/Storage/File/GetDownLoadFile',
     iconSrv: 'http://at.alicdn.com/t/font_1909561_ezgwtdkwuzi.js',
@@ -24,30 +28,6 @@
       ImEnable: true,
     },
     apps: [
-      {
-        name: 'platform',
-        hostParent: '#app-host-container',
-        hostClass: 'platform-layout',
-        routerPathPrefix: '/platform',
-        selector: 'platform-root',
-        resourcePathPrefix: '/apps/platform/',
-        manifest: '/apps/platform/manifest.json',
-        preload: true,
-        scripts: ['main.js'],
-        styles: ['styles.css'],
-      },
-      {
-        name: 'crm',
-        hostParent: '#app-host-container',
-        hostClass: 'crm-layout',
-        routerPathPrefix: '/crm',
-        selector: 'crm-root',
-        resourcePathPrefix: '/apps/crm/',
-        manifest: '/apps/crm/manifest.json',
-        preload: false,
-        scripts: ['main.js'],
-        styles: ['styles.css'],
-      },
       {
         name: 'fcm',
         hostParent: '#app-host-container',
