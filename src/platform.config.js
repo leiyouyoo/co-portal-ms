5e8fa25e2a2c2a4465a5861c63edc4ee27f307fc
youlei <yooleiyou@cityocean.com>
2020-12-18 12:06:35 +0800

    路线收藏样式优化

--- src/platform.config.js
+++ src/platform.config.js
@@ -27,7 +27,7 @@
     loginUrl: '/passport/login',
     uploadUrl: apiUrlPrefix + '/Storage/File/Upload',
     downloadUrl: apiUrlPrefix + '/Storage/File/GetDownLoadFile',
-    iconSrv: '//at.alicdn.com/t/font_1540363_luyqkcvbbu.js',
+    iconSrv: '//at.alicdn.com/t/font_1909561_c3urmr7ded7.js',
     excelDownloadUrl: apiUrlPrefix + '/Storage/Excel/DownloadExcel',
     appCityoceanUrl: 'https://test-app.cityocean.com',
     googleApiUrl: 'https://test-api.cityocean.com:10001',
